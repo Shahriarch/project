b84f6f896831e2acd495dd69c0f2d0bd79985a97
Shahriar Akbari <106760694+Shahriarch@users.noreply.github.com>
2023-05-24 16:33:44 +0200

    Update motion detection.py

--- motion detection.py
+++ motion detection.py
@@ -7,13 +7,15 @@ frame_height =int( cap.get( cv2.CAP_PROP_FRAME_HEIGHT))
 fourcc = cv2.VideoWriter_fourcc('X','V','I','D')
 
 out = cv2.VideoWriter(r"C:\Users\sakb\OneDrive - Det Kongelige Akademi\Desktop\Three Stages\motion.avi", fourcc, 5.0, (2160,3840))
-
-ret, frame1 = cap.read()
-ret, frame2 = cap.read()
+frame_id = 0 
+cap.set(cv2.CAP_PROP_POS_FRAMES, frame_id)
+frame1 = cv2.imread(r"C:\Users\sakb\OneDrive - Det Kongelige Akademi\Desktop\second monitoring framework\my_video_frame.png")
 
 print(frame1.shape)
 while cap.isOpened():
     #Image Processing
+    ret, frame2 = cap.read()
+    ret, frame3 = cap.read()
     blur1 = cv2.bilateralFilter(frame1,9,250,250)
     blur2 = cv2.bilateralFilter(frame2,9,250,250)
     diff = cv2.absdiff(blur1, blur2)
@@ -21,7 +23,7 @@ while cap.isOpened():
     _, thresh = cv2.threshold(gray, 50, 255, cv2.THRESH_BINARY)
     dilated = cv2.dilate(thresh, None, iterations=3)
     contours, _ = cv2.findContours(dilated, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    
+     
     for contour in contours:
         (x, y, w, h) = cv2.boundingRect(contour)
         if cv2.contourArea(contour) < 1000:
@@ -38,8 +40,8 @@ while cap.isOpened():
     gray = cv2.resize(gray, (465,738))
     thresh = cv2.resize(thresh, (465,738))
     dilated = cv2.resize(dilated, (465,738))
-    out.write(frame1)
-    
+    #out.write(frame1)
+    '''
     #Grid 
     image2 = image 
     H,W = image2.shape[:2]
@@ -54,15 +56,17 @@ while cap.isOpened():
 
     height = H/2 
     weight = W/2
+    '''
+
     cv2.imshow("blur", blur1)
-    cv2.imshow("grid", image2)
+    #cv2.imshow("grid", image2)
     cv2.imshow("feed", cv2.resize(frame1, (465,738)))
     cv2.imshow("difference", diff)
     cv2.imshow("binary", thresh)
     cv2.imshow("dilated", dilated)
 
     frame1 = frame2
-    ret, frame2 = cap.read()
+    ret, frame1 = cap.read()
 
     if cv2.waitKey(40) == 27:
         break
